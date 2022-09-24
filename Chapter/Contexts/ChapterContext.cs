71b35aea7e487b4e8422acf9d5cdf12bc05306db
carlalucio <103859003+carlalucio@users.noreply.github.com>
2022-09-24 10:49:19 -0300

    criação da API com a rota GET

---
+++ Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,25 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext() { }
+
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options) { }
+
+        //vamos utilizar esse método para configurar o banco de dados
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {//cada provedor tem sua sintaxe para especificação:path   ;    nome do DB          ;    segurança                     
+                optionsBuilder.UseSqlServer("Data Source=CL\\SQLEXPRESS; initial catalog=Chapter; Integrated Security=true");
+            }
+        }
+
+        //dbset representa as entidades q serão utilizadar nas operações de leitura, criação, atualização exclusão
+        public DbSet<Livro> Livros { get; set; }
+    }
+}

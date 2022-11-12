ee9b578dae94e904d684c70600cd9b787d937577
carlalucio <103859003+carlalucio@users.noreply.github.com>
2022-11-12 14:47:51 -0300

    Criação teste de integração

---
+++ TestProject1/Controller/LoginControllerTeste.cs
@@ -0,0 +1,71 @@
+﻿using Chapter.Controllers;
+using Chapter.Interfaces;
+using Chapter.Models;
+using Chapter.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace TestProject1.Controller
+{
+     public class LoginControllerTeste
+    {
+        [Fact]
+        public void LoginController_Retornar_Usuario_Invalido()
+        {
+            //Arrange
+            var fakeRepository = new Mock<IUsuarioRepository>();
+            fakeRepository.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns((Usuario)null);
+
+            LoginViewModel dadosLogin = new LoginViewModel();
+
+            dadosLogin.Email = "email@email.com";
+            dadosLogin.Senha = "123";
+
+            var controller = new LoginController(fakeRepository.Object);
+
+            //act
+            var resultado = controller.Login(dadosLogin);
+
+            //assert
+            Assert.IsType<UnauthorizedObjectResult>(resultado);
+        }
+
+        [Fact]
+        public void LoginController_Retornar_Token()
+        {
+            //Arrange
+            Usuario usuarioRetorno = new Usuario();
+            usuarioRetorno.Email = "email@email.com";
+            usuarioRetorno.Senha = "123";
+            usuarioRetorno.Tipo = "0";
+
+            var fakeRepository = new Mock<IUsuarioRepository>();
+            fakeRepository.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns(usuarioRetorno);
+
+            string issuerValidacao = "chapter.webapi";
+
+            LoginViewModel dadosLogin = new LoginViewModel();
+
+            dadosLogin.Email = "batata";
+            dadosLogin.Senha = "123";
+
+            var controller = new LoginController(fakeRepository.Object);
+
+            //act
+            OkObjectResult resultado = (OkObjectResult)controller.Login(dadosLogin);
+
+            string token = resultado.Value.ToString().Split(' ')[3];
+
+            var jwtHandler = new JwtSecurityTokenHandler();
+            var tokenJwt = jwtHandler.ReadJwtToken(token);
+
+            //assert
+            Assert.Equal(issuerValidacao, tokenJwt.Issuer);
+        }
+
+    }
+
+   
+
+}
